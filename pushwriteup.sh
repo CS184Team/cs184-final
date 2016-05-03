b6ec40eaa3f30ae00dcc0d3f18b7ba95d343ded6
zealng <zealng@berkeley.edu>
2016-05-03 13:36:28 -0700

    "script"

---
+++ pushwriteup.sh
@@ -0,0 +1,14 @@
+# pushes to "origin writeup"
+# you might need to modify that ^ on line 10
+if [[ $# -eq 0 ]] ; then
+    echo 'Pass in a message for the commit!:'
+    echo 'Ex)'
+    echo "'""\"Commit Message\"""'"
+    exit 0
+fi
+set -e
+git add -u
+# git status
+git commit -m $1
+git push origin HEAD:writeup
+# git status
