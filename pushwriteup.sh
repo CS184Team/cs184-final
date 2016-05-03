b151ac735765f18191bafc774601ff4f3f4197f5
zealng <zealng@berkeley.edu>
2016-05-03 14:04:35 -0700

    "Commit" "message"

--- pushwriteup.sh
+++ pushwriteup.sh
@@ -1,14 +1,20 @@
 # pushes to "origin writeup"
-# you might need to modify that ^ on line 13
+# you might need to modify that ^ on line 20
 if [[ $# -eq 0 ]] ; then
     echo 'Pass in a message for the commit!:'
-    echo 'Ex)'
-    echo "'""Commit Message""'"
+    echo 'Ex) -- (No Quotes Needed)'
+    echo "Commit Message"
     exit 0
 fi
+
+C=''
+for i in "$@"; do 
+    C="$C \"${i//\"/\\\"}\""
+done
+
 set -e
 git add -u
 # git status
-git commit -m $1
+git commit -m "$C"
 git push origin HEAD:writeup
 # git status
